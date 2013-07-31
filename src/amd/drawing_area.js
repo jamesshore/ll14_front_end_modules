7f85d1b2999cf34dd8802f5eb1c5f1dafcb22714
James Shore <jshore@jamesshore.com>
2013-07-31 16:13:25 -0700

    Updated copyright date

--- src/amd/drawing_area.js
+++ src/amd/drawing_area.js
@@ -1,4 +1,4 @@
-// Copyright (c) 2012 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
+// Copyright (c) 2013 Titanium I.T. LLC. All rights reserved. See LICENSE.txt for details.
 
 define(function(require, exports, module) {
 	"use strict";
